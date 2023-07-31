bdbd619feaf88b4b07084d0df305a5d86ada7dba
Máté Csizmadia <csizmadiamate22@gmail.com>
2023-07-31 18:46:33 +0200

    Exmpl. main file (mostly for workflow to build on)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
-#include "button.h"
-#include "led.h"
-#include "rgbled.h" 
+#include "utils/button.h"
+#include "utils/led.h"
+#include "utils/rgbled.h" 
+
+using namespace PTS;
+
+const LED led(LED_BUILTIN);
+const Button button(GPIO_NUM_5);
 
 void setup() {
-  // setup
+  led.begin();
+  //led.on();
+
+  button.begin();
+  //Serial.begin(9600);
+
+  button.onPressed([]() constexpr { led.on(); });
+  button.onReleased([]() constexpr { led.off(); });
+
+  //led.off();
 }
 
 void loop() {
+  button.update();
   // loop
 }
