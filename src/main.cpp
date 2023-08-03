01732b68e92ea6bb7d5e4643489c18a05375abc0
Máté Csizmadia <csizmadiamate22@gmail.com>
2023-08-03 02:36:06 +0200

    Remade the modul into an actual completeable game.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "module/basic_wire_disconnect.h"
 
-PTS::WireDisconnect module("module", GPIO_NUM_17, LED_BUILTIN);
+PTS::WireDisconnect module("module", GPIO_NUM_16, GPIO_NUM_17, GPIO_NUM_4);
 
 void setup() {
   Serial.begin(115200);
