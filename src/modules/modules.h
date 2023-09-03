3ffc0493326842f492efde75c70ef6dedd16ab41
Máté Csizmadia <csizmadiamate22@gmail.com>
2023-09-03 11:10:23 +0200

    Added Modules and Utils collection headers. They can be used to easily include all headers in the collection.

---
+++ src/modules/modules.h
@@ -0,0 +1,17 @@
+//===-- modules/modules.h - Module collection header ----------------------===//
+//
+// Project-Thunderstrike (PTS) collection header file.
+// Find more information at:
+// https://github.com/itsthatMatthew/Project-Thunderstrike
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file This file contains the include paths of the entire modules collection.
+///
+//===----------------------------------------------------------------------===//
+
+#include "modules/module_base.h"
+#include "modules/stateful_base.h"
+#include "modules/hw/blinker_module.h"
+#include "modules/hw/buzzer_module.h"
+#include "modules/hw/keypad_module.h"
