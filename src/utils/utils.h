03cbf5574e091074e0b9fe23d00197040ea51dc1
Máté Csizmadia <csizmadiamate22@gmail.com>
2023-10-03 10:37:44 +0200

    Added log.h to the utils utils collection header.

---
+++ src/utils/utils.h
@@ -0,0 +1,18 @@
+//===-- modules/modules.h - Utils collection header -----------------------===//
+//
+// Project-Thunderstrike (PTS) collection header file.
+// Find more information at:
+// https://github.com/itsthatMatthew/Project-Thunderstrike
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file This file contains the include paths of the entire utils collection.
+///
+//===----------------------------------------------------------------------===//
+
+#include "utils/hw/button.h"
+#include "utils/hw/led.h"
+#include "utils/hw/rgbled.h"
+#include "utils/hw/status_bar.h"
+#include "utils/sw/circular_buffer.h"
+#include "utils/sw/log.h"
